224d706a7f1a97ca0795d32c49c0e8287cc123c0
JozefRepka <jozo.repka@gmail.com>
2024-06-29 13:41:58 +0200

    Added model Company & Executive.

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.db.models import Model, CharField, IntegerField, ForeignKey, ManyToManyField, DateTimeField, DateField
+
+
+class Company(Model):
+    name = CharField(max_length=50)
+    date_of_foundation = DateField(null=True)
+    #registered_office = ManyToManyField("RegisteredOffice", blank=True, related_name="company")
+    #employee = ForeignKey(Employee, on_delete=DO_NOTHING)
+    #employee je väzba company-employee one to many
+
+
+class Executive(Model):
+    name = CharField(max_length=50)
+    address = CharField(max_length=50)
+    date_of_change = DateField(null=True)
+
+# class RegisteredOffice(Model):
+#     address = CharField(max_length=20)
+#
+#
+# class Employee(Model):
+#     employee_count = IntegerField(default=0)
+#
+#
+# class Revenue(Model):
+#     yearly_revenue = IntegerField(default=0)
+#     year = IntegerField(default=0)
+#
+#
+# class Company_debt(Model):
+#     tax_office_debt = IntegerField(default=0)
+#     social_insurance_debt = IntegerField(default=0)
+#     health_insurance_debt = IntegerField(default=0)
