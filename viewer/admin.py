bfd100f845e89aa9c240c2a68c5a5bef6e72d7c3
Martin Kališ <kkalis.martin@gmail.com>
2024-07-07 08:13:12 +0200

    Admin panel

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from viewer.models import Company, Executive
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Executive)
--- viewer/models.py
+++ viewer/models.py
@@ -11,11 +11,17 @@ class Company(Model):
     #employee = ForeignKey(Employee, on_delete=DO_NOTHING)
     #employee je väzba company-employee one to many
 
+    def __str__(self):
+        return self.name
+
 
 class Executive(Model):
     name = CharField(max_length=50)
     address = CharField(max_length=50)
     date_of_change = DateField(null=True)
+
+    def __str__(self):
+        return self.name
 
 # class RegisteredOffice(Model):
 #     address = CharField(max_length=20)
