d4d5fd7ac529f991cbf4070b3ab19d562f8836f7
Martin Kališ <kkalis.martin@gmail.com>
2024-07-14 13:54:10 +0200

    Base template

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from viewer.models import Company
+
+
 # Create your views here.
+def companies(request):
+    result = Company.objects.all()
+    context = {'companies': result}
+    return render(
+        request,
+        'database.html',
+        context
+    )
+
+
+def home(request):
+    return render(request, 'home.html', {'title': 'Welcome to SafeBusiness'})
+
+
+def about_us(request):
+    return render(request, 'about_us.html', {'title': 'Welcome to SafeBusiness'})
